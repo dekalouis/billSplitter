d1e500d0a2ca4dddbf45e7d43946deebd649ad81
dekalouis <dekarebaudo@gmail.com>
2025-02-25 23:25:47 +0700

    fix: fixed 3p API .env failure.

--- helpers/imagekit.js
+++ helpers/imagekit.js
@@ -7,8 +7,14 @@
 // });
 
 // module.exports = imagekit;
-
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 const ImageKit = require("imagekit");
+
+// console.log("IMAGEKIT_PUBLIC_KEY:", process.env.IMAGEKIT_PUBLIC_KEY);
+// console.log("IMAGEKIT_PRIVATE_KEY:", process.env.IMAGEKIT_PRIVATE_KEY);
+// console.log("IMAGEKIT_URL_ENDPOINT:", process.env.IMAGEKIT_URL_ENDPOINT);
 
 let imagekit;
 
